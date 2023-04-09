9a909e8a9046a925b2668ad08165b9454ee93cf6
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-04-09 13:11:03 -0400

    adding some websites

---
+++ portfoliosis/src/components/pages/About.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+const About = () => {
+  return <>  <section id="hero">
+  <h1 class="label">About me</h1>
+  <div id="aboutme">
+    <p>
+      <h1>Intro: Hello-World!;</h1>
+      My name is Benjamin Sprenger. Problem solving, Quality and UI Design are things I am passionate about.
+    </p>
+  </div>
+</section></>;
+};
+
+export default About;
---
+++ portfoliosis/src/components/pages/Contact.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Form from './components/Form/index';
+
+function Contact() {
+  return <Form />;
+}
+
+export default Contact;
